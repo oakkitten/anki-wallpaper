d7a9fc12c1c48436d2127c5e2bc12c0eb75748df
oakkitten <oakkitten@users.noreply.github.com>
2022-05-02 14:06:13 +0100

    Tests: test Add cards dialog

--- tests/test_wallpaper.py
+++ tests/test_wallpaper.py
@@ -1,7 +1,7 @@
 import aqt
-from aqt.qt import QColor, QWindow, QMainWindow
+from aqt.qt import QColor, QWindow, QWidget, QDialog
 
-from tests.conftest import wait_until
+from tests.conftest import wait_until, wait
 
 
 def get_color(window: QWindow, x: int, y: int) -> str:
@@ -9,7 +9,7 @@ def get_color(window: QWindow, x: int, y: int) -> str:
     return QColor(color).name()
 
 
-def set_main_window_dimensions(window: QMainWindow, width: int, height: int):
+def set_window_dimensions(window: QWidget, width: int, height: int):
     window.setFixedWidth(width)
     window.setFixedHeight(height)
 
@@ -18,9 +18,21 @@ def set_main_window_dimensions(window: QMainWindow, width: int, height: int):
 
 
 def test_main_window(setup):
-    set_main_window_dimensions(aqt.mw, 500, 500)
-    wait_until(lambda: get_color(aqt.mw, 5, 47) == "#aaaaaa")  # gray line below upper links
-    assert get_color(aqt.mw, 5, 5) == "#eeebe7"  # menu
-    assert get_color(aqt.mw, 5, 40) == "#eeebe7"  # links
-    assert get_color(aqt.mw, 5, 280) == "#eeebe7"  # main area
-    assert get_color(aqt.mw, 5, 490) == "#e5dfd9"  # bottom area
+    window = aqt.mw
+    set_window_dimensions(window, 500, 500)
+    wait_until(lambda: get_color(window, 5, 47) == "#aaaaaa")  # gray line below upper links
+    assert get_color(window, 5, 5) == "#eeebe7"  # menu
+    assert get_color(window, 5, 40) == "#eeebe7"  # links
+    assert get_color(window, 5, 280) == "#eeebe7"  # main area
+    assert get_color(window, 5, 490) == "#e5dfd9"  # bottom area
+
+
+def test_add_cards_dialog(setup):
+    dialog = aqt.dialogs.open("AddCards", aqt.mw)
+    set_window_dimensions(dialog, 500, 500)
+    wait_until(lambda: get_color(dialog, 330, 160) == "#ffffff")  # field1
+    assert get_color(dialog, 5, 5) == "#eeebe7"  # edge
+    assert get_color(dialog, 270, 80) == "#eeebe7"  # buttons area
+    assert get_color(dialog, 270, 270) == "#eeebe7"  # main area
+    assert get_color(dialog, 270, 430) == "#eeebe7"  # tags area
+    assert get_color(dialog, 5, 490) == "#e5dfd9"  # bottom area
